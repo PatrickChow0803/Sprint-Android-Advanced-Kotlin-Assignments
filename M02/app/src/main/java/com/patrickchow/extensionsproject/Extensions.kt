09eefa5e9dd454479dc73883df2bc6eb0abefceb
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-15 17:27:18 -0400

    Added extension function for notification

---
+++ M02/app/src/main/java/com/patrickchow/extensionsproject/Extensions.kt
@@ -0,0 +1,28 @@
+package com.patrickchow.extensionsproject
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+
+fun Context.notification(title: String = "Notification Title", message:String, notifyId: Int, channelId: String = ""){
+    val notificationBuilder = NotificationCompat.Builder(this, channelId)
+    notificationBuilder.setContentTitle(title)
+        .setPriority(NotificationManager.IMPORTANCE_HIGH)
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .setColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+        .setContentText(message)
+        .setDefaults(Notification.DEFAULT_ALL)
+
+    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        val name = "Notifications Channel"
+        val notificationChannel = NotificationChannel(channelId, name, NotificationManager.IMPORTANCE_DEFAULT)
+        notificationChannel.description = "Notification channel description"
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+    notificationManager.notify(notifyId, notificationBuilder.build())
+}
