122810a7a4ce672ef0acbcbe057bea9d892e1708
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-17 13:03:05 -0400

    Added dependencies and basic conductor

--- M04/Guided-Project/app/src/main/java/com/patrickchow/conductor/MainActivity.kt
+++ M04/Guided-Project/app/src/main/java/com/patrickchow/conductor/MainActivity.kt
@@ -2,11 +2,36 @@ package com.patrickchow.conductor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
+import com.patrickchow.conductor.Controllers.HomeController
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var router: Router
+    private val container: ViewGroup by lazy {
+        this.findViewById<ViewGroup>(R.id.cl_activity_main_parent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)d
+        setContentView(R.layout.activity_main)
+
+        router = Conductor.attachRouter(this, container, savedInstanceState)
+        if(!router.hasRootController()){
+            router.setRoot(RouterTransaction.with(HomeController()))
+        }
+
     }
+
+    override fun onBackPressed() {
+        //If router can't handel back button, then use onBackPressed
+        if(!router.handleBack()){
+            super.onBackPressed()
+        }
+    }
+
 }
